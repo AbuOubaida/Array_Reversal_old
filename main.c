593e2fb87c3ef8f16cdc69433f886e1d2004fa44
Abu Oubaida <34217170+AbuOubaida@users.noreply.github.com>
2022-03-21 15:56:18 +0600

    Update main.c

--- main.c
+++ main.c
@@ -3,36 +3,24 @@
 
 int main()
 {
-    int num, *arr /*, arr2*/, i;
+    int num, *arr , *arr2, i;
     scanf("%d", &num);
     arr = (int*) malloc(num * sizeof(int));
-    for(i = 0; i < num; i++) {
+    for(i = 0; i < num; i++) 
+    {
         scanf("%d", arr + i);
     }
-
-
-    /* Write the logic to reverse the array. */
 //Way 1
-//    arr2 = (int*) malloc(num * sizeof(int));
-//    for(i = 0; i < num; i++)
-//    {
-//        arr2[i] = * (arr + ((num-1)-i));
-//    }
-//    arr = arr2;
-//-------------------------------
-//Way 2
-    int temp = 0;
-    int size = (int) (num/2);
-    //printf("Output\n");
-    for(i = 0; i < size; i++)
+    arr2 = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++)
     {
-         temp = *(arr + i);
-         arr[i] = * (arr + ((num-1)-i));
-         arr[(num-1)-i] = temp;
+       arr2[i] = * (arr + ((num-1)-i));
     }
+    arr = arr2;
 //-------------------------------
     for(i = 0; i < num; i++)
         printf("%d ", *(arr + i));
     free(arr);
+    free(arr2);
     return 0;
 }
